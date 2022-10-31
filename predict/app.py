3bb08b5ad263a60d9a45775d9cea770e47de1be3
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2022-10-31 13:55:09 +0900

    Add predict code

--- predict/app.py
+++ predict/app.py
@@ -1,9 +1,32 @@
 import gradio as gr
+import numpy
+import onnxruntime
 
 
-def greet(name):
+def predict(image):
+    reshaped_arr = image.reshape(-1)
+    print("reshaped_arr")
+    print(reshaped_arr)
+
+    input = [reshaped_arr.astype(numpy.float32)]
+    print("input")
+    print(input)
+
+    # predict
+
+    onnx_session = onnxruntime.InferenceSession("../train/model.onnx")
+
+    output = onnx_session.run(['probabilities'], {'float_input': input})
+    print("output")
+    print(output)
+
+    result = output[0][0]
+    print("result")
+    print(result)
+
+    name = "world"
     return "Hello " + name + "!"
 
-demo = gr.Interface(fn=greet, inputs="text", outputs="text")
+demo = gr.Interface(fn=predict, inputs="sketchpad", outputs="text")
 
 demo.launch()
