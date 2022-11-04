92eb6066c8b36a3eac4b19f28cc5d7cfdd62e5a9
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2022-11-04 19:01:00 +0900

    Add gradio_hello.py

--- predict/gradio_app.py
+++ predict/gradio_app.py
@@ -1,10 +1,10 @@
-import gradio
+import gradio as gr
 
 from predict import predict
 
-app = gradio.Interface(title="Handwritten Number Prediction",
-                       fn=predict, inputs="sketchpad", outputs="label",
-                       live=True, allow_flagging="never",
-                       css=".gradio-container { max-width: 800px; margin: 0 auto; }")
+app = gr.Interface(title="Handwritten Number Prediction",
+                   fn=predict, inputs="sketchpad", outputs="label",
+                   live=True, allow_flagging="never",
+                   css=".gradio-container { max-width: 800px; margin: 0 auto; }")
 
 app.launch()
